3b777eedde42ac3b306394b08d0f1862644e1cc5
ameya-6964 <belvalkar7@gmail.com>
2024-01-22 00:56:32 +0530

    Created Product Controller and Route

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,10 @@ dotenv.config({ path: "backend/config/config.env" });
 // Create an instance of the Express application
 const app = express();
 
+// Import all routes
+import productRoutes from "./routes/products.js";
+app.use("/api/v1", productRoutes);
+
 app.listen(process.env.PORT, () => {
   console.log(
     `Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
