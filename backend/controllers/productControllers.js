0bf75acf3fb683152be95ad1ec8ae94a2be9c24d
ameya-6964 <belvalkar7@gmail.com>
2024-01-22 01:02:54 +0530

    Added Proper Comments

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,4 +1,6 @@
+// Retrieves all products.
 export const getProducts = async (req, res) => {
+  // Set the response status to 200 and send a JSON object with the message "All Products".
   res.status(200).json({
     message: "All Products",
   });
