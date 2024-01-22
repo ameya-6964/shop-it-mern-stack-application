1bd7c8a1661815b052406b1a29e74489494f9db0
ameya-6964 <belvalkar7@gmail.com>
2024-01-22 14:36:00 +0530

    Added Proper Comments

--- backend/middlewares/catchAsyncErrors.js
+++ backend/middlewares/catchAsyncErrors.js
@@ -1,2 +1,3 @@
+// Middleware wrapper for error handling with Promises
 export default (controllerFunction) => (req, res, next) =>
   Promise.resolve(controllerFunction(req, res, next)).catch(next);
--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -1,6 +1,8 @@
+// Global error handler middleware
 import ErrorHandler from "../utils/errorHandler.js";
 
 export default (err, req, res, next) => {
+  // Initialize the error object with default values
   let error = {
     statusCode: err?.statusCode || 500,
     message: err?.message || "Internal Server Error",
@@ -8,16 +10,21 @@ export default (err, req, res, next) => {
 
   // Handle Invalid Mongoose ID Error
   if (err.name === "CastError") {
+    // If the error is a Mongoose CastError, create a new error with a specific message and status code
     const message = `Resource not found. Invalid: ${err?.path}`;
     error = new ErrorHandler(message, 404);
   }
 
   // Handle Validation Error
   if (err.name === "ValidationError") {
+    // If the error is a Mongoose ValidationError, extract error messages and set a new status code
     const message = Object.values(err.errors).map((value) => value.message);
     error = new ErrorHandler(message, 400);
   }
 
+  // Respond with error details based on the environment
+
+  // In development environment, provide detailed error information including the error object and stack trace
   if (process.env.NODE_ENV === "DEVELOPMENT") {
     res.status(error.statusCode).json({
       message: error.message,
@@ -26,6 +33,7 @@ export default (err, req, res, next) => {
     });
   }
 
+  // In production environment, provide a simplified response with only the error message
   if (process.env.NODE_ENV === "PRODUCTION") {
     res.status(error.statusCode).json({
       message: error.message,
